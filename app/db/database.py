8aacee4367db73ffa641ed568214f02c1afa55a0
Neyder-Dev <naquemba@ucompensar.edu.co>
2025-11-17 17:06:44 -0500

    Migración a Postgres para Render

--- app/db/database.py
+++ app/db/database.py
@@ -1,25 +1,12 @@
-import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from app.core.config import settings
 
-# Render provee DATABASE_URL automáticamente
-SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
-
-if not SQLALCHEMY_DATABASE_URL:
-    raise ValueError("DATABASE_URL environment variable not set")
-
-# Si Render usa mysql:// lo convertimos a mysql+pymysql://
-if SQLALCHEMY_DATABASE_URL.startswith("mysql://"):
-    SQLALCHEMY_DATABASE_URL = SQLALCHEMY_DATABASE_URL.replace(
-        "mysql://",
-        "mysql+pymysql://",
-        1
-    )
+SQLALCHEMY_DATABASE_URL = settings.DATABASE_URL
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL,
-    pool_pre_ping=True,
-    pool_recycle=280,
+    pool_pre_ping=True
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
